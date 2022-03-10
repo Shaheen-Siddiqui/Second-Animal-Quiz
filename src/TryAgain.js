108e56e59f0d9f57eb1d5855f4ab390333f16982
Shaheen-Siddiqui <95218912+Shaheen-Siddiqui@users.noreply.github.com>
2022-03-10 08:53:28 +0530

    thirteen commit

--- src/TryAgain.js
+++ src/TryAgain.js
@@ -9,7 +9,7 @@ const TryAgain = ({scoreCount,setQuestionCount,NextData,QuestionCount}) => {
     <div>
 <div className="TryagainBox">
  <h1>**!!GAME ENDED!!**</h1>
- <h2 className='Chances'>Chances over you did very Well</h2> <br />
+ <h2 className='Chances'>Chances over you did Well</h2> <br />
  <h4 className='playaGain'><strong>You got {scoreCount} score out of {QuestionBank.length} </strong> </h4>
  <a href='/Second-Animal-Quiz/redirect'><button className='tryAgainBtn btn btn-danger'>Play again </button></a> 
  </div>
