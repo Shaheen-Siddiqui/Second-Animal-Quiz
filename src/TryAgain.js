b1deb4d848c570d94fb9db62540f89a509ab4e11
Shaheen-Siddiqui <95218912+Shaheen-Siddiqui@users.noreply.github.com>
2022-03-10 07:47:30 +0530

    ten commit

--- src/TryAgain.js
+++ src/TryAgain.js
@@ -11,7 +11,7 @@ const TryAgain = ({scoreCount,setQuestionCount,NextData,QuestionCount}) => {
  <h1>**!!GAME ENDED!!**</h1>
  <h2 className='Chances'>Chances over you did very Well</h2> <br />
  <h4 className='playaGain'><strong>You got {scoreCount} score out of {QuestionBank.length} </strong> </h4>
- <a href='redirect'><button className='tryAgainBtn btn btn-danger'>Play again </button></a> 
+ <a href='/Second-Animal-Quiz/redirect'><button className='tryAgainBtn btn btn-danger'>Play again </button></a> 
  </div>
     </div>
   )
